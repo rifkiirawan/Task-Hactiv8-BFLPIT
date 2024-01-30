867b07b4ed12f1acf36ffb4716b6c7eab5a8704a
Rifki <Rifki@Rifki>
2024-01-30 11:32:24 +0700

    upload challenge 3

---
+++ Tugas Day 2/Challenge3.js
@@ -0,0 +1,15 @@
+var a = 6
+console.log(cetakSegitiga(a))
+function cetakSegitiga(a){
+    for (var i=1-1;i<=a;i++){
+        var space = " ";
+        var symbol ="";
+
+        for(var x=i;x>0;x--){
+            if(x%2==0){
+                symbol += "#";
+            }else symbol += "*"
+        }
+        console.log (space.repeat(a-i)+symbol);
+    }
+}
